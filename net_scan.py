f4c3b400eca44e3ba3b1ec8f0d12b799a795edc3
Jewles <49858956+Jewles@users.noreply.github.com>
2021-07-16 10:32:24 +0800

    Update net_scan.py

--- net_scan.py
+++ net_scan.py
@@ -7,11 +7,15 @@ ip_seg = input("IP Address:")
 s.scan(hosts=ip_seg, arguments='-n -sP')
 s_all.scan(hosts=ip_seg, arguments='-n -sL')
 
-print("IP_段总数:\n", len(s_all.all_hosts()))
-print("存活_IP总数：\n", len(s.all_hosts()))
-print("存活_IP List：\n", s.all_hosts())
-
-
 s_fail = list(set(s_all.all_hosts()) - set(s.all_hosts()))
+
+print("该段IP地址总数:\n", len(s_all.all_hosts()))
+print("存活_IP总数：\n", len(s.all_hosts()))
 print("不在线_IP总数:\n", len(s_fail))
-print("不在线_IP List：\n", s_fail)
+
+print("存活IP列表：")
+for upip in s.all_hosts():
+    print(upip)
+print("不在线IP列表：")
+for noip in s_fail:
+    print(noip)
