475031381830097b4756f0a0702704674d322324
nokk <49858956+Jewles@users.noreply.github.com>
2023-07-24 19:05:39 +0800

    Update net_scan.py

--- net_scan.py
+++ net_scan.py
@@ -1,4 +1,6 @@
+import sys
 import nmap
+import os
 
 def scan_ips():
     s = nmap.PortScanner()
@@ -13,16 +15,18 @@ def scan_ips():
     print("存活_IP总数：\n", len(s.all_hosts()))
     print("不在线_IP总数:\n", len(s_fail))
 
-    print("\033[1;32m存活IP列表：")
+    print("存活IP列表：\033[1;32m")
     for upip in s.all_hosts():
         print(upip)
-    print("\033[1;31m不在线IP列表：")
+    print("\033[0m不在线IP列表：\033[1;31m")
     for noip in s_fail:
         print(noip)
 
 if __name__ == "__main__":
     while True:
         scan_ips()
-        restart = input("是否扫描其他地址(yes/on)：")
-        if restart.lower()!= "yes":
-            break
+        restart = input("\033[1;33m继续扫描输入yes，关闭程序输入no(yes/no)：")
+        if restart.lower() != "yes":
+            sys.stdout.flush()
+            sys.stderr.flush()
+            os._exit(0)
